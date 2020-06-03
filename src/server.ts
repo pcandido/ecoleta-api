94e81b5e950a2d802e4d1f4704458847c38fba9a
Paulo Cândido <pcandido.m@gmail.com>
2020-06-02 22:48:30 -0300

    imagens estaticas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import figlet from 'figlet'
 import router from './routes'
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 const port = 3000
 
